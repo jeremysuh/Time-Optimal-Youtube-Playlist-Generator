48d780b3ec1ca1df737a1a64deeb0e8446b90428
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-04-29 20:45:53 +0900

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Time Optimal Youtube PLaylist Generator</div>
+    <div className="App">Time Optimal Youtube Playlist Generator</div>
   );
 }
 
