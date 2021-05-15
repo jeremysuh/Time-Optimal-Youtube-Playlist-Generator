19b348489b06b04eaf6b56fba3d0a2ab05009d1a
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-05-15 16:24:57 +0900

    Update logic order

--- server/server.ts
+++ server/server.ts
@@ -17,17 +17,7 @@ app.get("/", (req, res) => {
   res.send("Root");
 });
 
-//updates
-//1: dont do the data appending in the then; do it outside
-//2: continue to add all videos using the nextToken
-
-app.get("/playlist", async (req, res) => {
-
-  res.set("Content-Type",'application/json');
-
-  const playlistId: string | undefined = req.query.playlistId as string | undefined;
-  if (!playlistId) res.send("No valid playlistId included");
-
+const retrieveVideoIdsFromPlaylist = async(playlistId: string): Promise<string[]>  => {
   const url = "https://youtube.googleapis.com/youtube/v3/playlistItems";
 
   const params = new URLSearchParams();
@@ -38,45 +28,90 @@ app.get("/playlist", async (req, res) => {
   params.append("playlistId", playlistId as string);
   params.append("key", process.env.YOUTUBE_API_KEY as string);
 
-  const info = await axios.get(url, { params })
-    .then((response: AxiosResponse) => response.data)
+  const info = await axios.get(url, { params }).then((response: AxiosResponse) => response.data);
   const totalResults = info.pageInfo.totalResults;
   const resultsPerPage = info.pageInfo.resultsPerPage;
 
-  const pageDatas : any[] = []
-  let nextPageToken : string = "N/A"
+  const pageDatas: any[] = [];
+  let nextPageToken: string = "N/A";
 
   for (let i = 0; i < Math.ceil(totalResults / resultsPerPage); ++i) {
     params.delete("pageToken");
     if (nextPageToken !== "N/A") params.append("pageToken", nextPageToken);
-    const perData = await axios.get(url, { params }).then((response: AxiosResponse) => response.data)
-    if (perData.hasOwnProperty('nextPageToken')) {
+    const perData = await axios.get(url, { params }).then((response: AxiosResponse) => response.data);
+    if (perData.hasOwnProperty("nextPageToken")) {
       nextPageToken = perData.nextPageToken;
-    }else{
+    } else {
       nextPageToken = "N/A";
     }
-    pageDatas.push(perData)
+    pageDatas.push(perData);
   }
 
-  const videos: any[] = [];
+  const videos: string[] = [];
 
-  pageDatas.forEach((data) => 
-    {    
-      const items: any[] = data.items;
-    
-      items.forEach((item: any) => {
-        const snippet: any = item.snippet;
-        const contentDetails: any = item.contentDetails;
-        videos.push({
-          index: videos.length,
-          title: snippet.title,
-          date: contentDetails.videoPublishedAt,
-          id: contentDetails.videoId,
-        });
-      });
+  pageDatas.forEach((data) => {
+    const items: any[] = data.items;
+
+    items.forEach((item: any) => {
+      const snippet: any = item.snippet;
+      const contentDetails: any = item.contentDetails;
+      videos.push(
+        contentDetails.videoId
+      );
+    });
+  });
+
+  return videos;
+}
+
+const retrieveVideosFromIds = async(videoIds: string[]): Promise<YoutubeVideo[]> => {
+  const videos : YoutubeVideo[] = [];
+  videos.push(
+    {
+      title: "test",
+      id: "test",
+      channelTitle: "test",
+      channelId: "test",
+      stats: null
     }
   )
-  res.json(videos);
+  return videos;
+}
+
+const collectAllVideos = async (playlistId: string): Promise<YoutubeVideo[]> => {
+  const videoIds : string[] = await retrieveVideoIdsFromPlaylist(playlistId);
+  const videos : YoutubeVideo[] = await retrieveVideosFromIds(videoIds);
+  return videos;
+};
+
+const determineOptiminalPlaylist = async (videos: YoutubeVideo[], preference: UserPreference): Promise<YoutubeVideo[] | null> => {
+  return videos;
+};
+
+const PRIORITY = {
+  NONE : "none",
+  VIEWS_MANY: "views_many",
+  VIEWS_FEW: "views_few",
+  LIKES: "likes",
+  DISLIKES: "dislikes",
+  NEW: "new",
+  OLD: "old"
+}
+
+app.get("/playlist", async (req, res) => {
+  res.set("Content-Type", "application/json");
+
+  const playlistId: string | undefined | null = req.query.playlistId as string | undefined | null;
+  if (!playlistId || playlistId == null) res.send("No valid playlistId included");
+
+  const preference : UserPreference = {time: 30, priority: PRIORITY.NONE}
+  
+  const videos = await collectAllVideos(playlistId as string);
+  const playlist = await determineOptiminalPlaylist(videos as YoutubeVideo[], preference);
+
+  const videoIdsTemp = await retrieveVideoIdsFromPlaylist(playlistId as string)
+
+  res.json(videoIdsTemp);
 });
 
 app.listen(PORT, () => {
