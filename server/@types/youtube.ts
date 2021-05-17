9cc63db51dbdfb87852fca588856bc201aae6d1e
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-05-17 16:26:49 +0900

    Define Youtube API types

--- server/@types/youtube.ts
+++ server/@types/youtube.ts
@@ -1,22 +1,72 @@
+type PlaylistItem = {
+    kind: "youtube#playlistItem";
+    etag: string; //etag
+    id: string;
+    snippet: {
+        publishedAt: Date;
+        channelId: string;
+        title: string;
+        description: string;
+        thumbnails: {
+            [key: string]: {
+                url: string;
+                width: number;
+                height: number;
+            };
+        };
+        channelTitle: string;
+        videoOwnerChannelTitle: string;
+        videoOwnerChannelId: string;
+        playlistId: string;
+        position: number;
+        resourceId: {
+            kind: string;
+            videoId: string;
+        };
+    };
+    contentDetails: {
+        videoId: string;
+        startAt: string;
+        endAt: string;
+        note: string;
+        videoPublishedAt: Date;
+    };
+    status: {
+        privacyStatus: string;
+    };
+};
+
+type PlaylistItemListResponse = {
+    kind: "youtube#playlistItemListResponse";
+    etag: string;
+    nextPageToken: string;
+    prevPageToken: string;
+    pageInfo: {
+        totalResults: number;
+        resultsPerPage: number;
+    };
+    items: PlaylistItem[];
+};
+
 type YoutubeVideoStats = {
-  duration: number; //weight
-  publishedAt: Date; //value
-  viewCount: number; //value
-  likeCount: number; //value
-  dislikeCount: number; //value
+    duration: number; //weight
+    publishedAt: Date; //value
+    viewCount: number; //value
+    likeCount: number; //value
+    dislikeCount: number; //value
 };
 
 type YoutubeVideo = {
-  title: string;
-  id: string;
-  channelTitle: string;
-  channelId: string;
-  stats: YoutubeVideoStats | null;
+    title: string;
+    id: string;
+    channelTitle: string;
+    channelId: string;
+    stats: YoutubeVideoStats | null;
 };
 
 type UserPreference = {
     time: number;
     priority: string;
-}
+};
 
-export type {YoutubeVideo, UserPreference}
+export type { YoutubeVideo, UserPreference, PlaylistItem, PlaylistItemListResponse };
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,12 @@
 import express, { json, response } from "express";
 import cors from "cors";
 import { AxiosResponse } from "axios";
-import { YoutubeVideo, UserPreference } from "./@types/youtube";
+import {
+  YoutubeVideo,
+  UserPreference,
+  PlaylistItem,
+  PlaylistItemListResponse,
+} from "./@types/youtube";
 
 require("dotenv").config();
 
@@ -17,7 +22,9 @@ app.get("/", (req, res) => {
   res.send("Root");
 });
 
-const retrieveVideoIdsFromPlaylist = async(playlistId: string): Promise<string[]>  => {
+const retrieveVideoIdsFromPlaylist = async (
+  playlistId: string
+): Promise<string[]> => {
   const url = "https://youtube.googleapis.com/youtube/v3/playlistItems";
 
   const params = new URLSearchParams();
@@ -28,17 +35,21 @@ const retrieveVideoIdsFromPlaylist = async(playlistId: string): Promise<string[]
   params.append("playlistId", playlistId as string);
   params.append("key", process.env.YOUTUBE_API_KEY as string);
 
-  const info = await axios.get(url, { params }).then((response: AxiosResponse) => response.data);
+  const info: PlaylistItemListResponse = await axios
+    .get(url, { params })
+    .then((response: AxiosResponse) => response.data);
   const totalResults = info.pageInfo.totalResults;
   const resultsPerPage = info.pageInfo.resultsPerPage;
 
-  const pageDatas: any[] = [];
+  const pageDatas: PlaylistItemListResponse[] = [];
   let nextPageToken: string = "N/A";
 
   for (let i = 0; i < Math.ceil(totalResults / resultsPerPage); ++i) {
     params.delete("pageToken");
     if (nextPageToken !== "N/A") params.append("pageToken", nextPageToken);
-    const perData = await axios.get(url, { params }).then((response: AxiosResponse) => response.data);
+    const perData: PlaylistItemListResponse = await axios
+      .get(url, { params })
+      .then((response: AxiosResponse) => response.data);
     if (perData.hasOwnProperty("nextPageToken")) {
       nextPageToken = perData.nextPageToken;
     } else {
@@ -48,68 +59,79 @@ const retrieveVideoIdsFromPlaylist = async(playlistId: string): Promise<string[]
   }
 
   const videos: string[] = [];
+  console.log(pageDatas[0]);
 
-  pageDatas.forEach((data) => {
-    const items: any[] = data.items;
+  pageDatas.forEach((data: PlaylistItemListResponse) => {
+    const items: PlaylistItem[] = data.items;
 
-    items.forEach((item: any) => {
-      const snippet: any = item.snippet;
-      const contentDetails: any = item.contentDetails;
-      videos.push(
-        contentDetails.videoId
-      );
+    items.forEach((item: PlaylistItem) => {
+      const snippet = item.snippet;
+      const contentDetails = item.contentDetails;
+      videos.push(contentDetails.videoId);
     });
   });
 
   return videos;
-}
+};
 
-const retrieveVideosFromIds = async(videoIds: string[]): Promise<YoutubeVideo[]> => {
-  const videos : YoutubeVideo[] = [];
-  videos.push(
-    {
-      title: "test",
-      id: "test",
-      channelTitle: "test",
-      channelId: "test",
-      stats: null
-    }
-  )
-  return videos;
-}
-
-const collectAllVideos = async (playlistId: string): Promise<YoutubeVideo[]> => {
-  const videoIds : string[] = await retrieveVideoIdsFromPlaylist(playlistId);
-  const videos : YoutubeVideo[] = await retrieveVideosFromIds(videoIds);
+const retrieveVideosFromIds = async (
+  videoIds: string[]
+): Promise<YoutubeVideo[]> => {
+  const videos: YoutubeVideo[] = [];
+  videos.push({
+    title: "test",
+    id: "test",
+    channelTitle: "test",
+    channelId: "test",
+    stats: null,
+  });
   return videos;
 };
 
-const determineOptiminalPlaylist = async (videos: YoutubeVideo[], preference: UserPreference): Promise<YoutubeVideo[] | null> => {
+const collectAllVideos = async (
+  playlistId: string
+): Promise<YoutubeVideo[]> => {
+  const videoIds: string[] = await retrieveVideoIdsFromPlaylist(playlistId);
+  const videos: YoutubeVideo[] = await retrieveVideosFromIds(videoIds);
+  return videos;
+};
+
+const determineOptiminalPlaylist = async (
+  videos: YoutubeVideo[],
+  preference: UserPreference
+): Promise<YoutubeVideo[] | null> => {
   return videos;
 };
 
 const PRIORITY = {
-  NONE : "none",
+  NONE: "none",
   VIEWS_MANY: "views_many",
   VIEWS_FEW: "views_few",
   LIKES: "likes",
   DISLIKES: "dislikes",
   NEW: "new",
-  OLD: "old"
-}
+  OLD: "old",
+};
 
 app.get("/playlist", async (req, res) => {
   res.set("Content-Type", "application/json");
 
-  const playlistId: string | undefined | null = req.query.playlistId as string | undefined | null;
-  if (!playlistId || playlistId == null) res.send("No valid playlistId included");
+  const playlistId: string | undefined | null = req.query.playlistId as
+    | string
+    | undefined
+    | null;
+  if (!playlistId || playlistId == null)
+    res.send("No valid playlistId included");
 
-  const preference : UserPreference = {time: 30, priority: PRIORITY.NONE}
-  
+  const preference: UserPreference = { time: 30, priority: PRIORITY.NONE };
+
   const videos = await collectAllVideos(playlistId as string);
-  const playlist = await determineOptiminalPlaylist(videos as YoutubeVideo[], preference);
+  const playlist = await determineOptiminalPlaylist(
+    videos as YoutubeVideo[],
+    preference
+  );
 
-  const videoIdsTemp = await retrieveVideoIdsFromPlaylist(playlistId as string)
+  const videoIdsTemp = await retrieveVideoIdsFromPlaylist(playlistId as string);
 
   res.json(videoIdsTemp);
 });
