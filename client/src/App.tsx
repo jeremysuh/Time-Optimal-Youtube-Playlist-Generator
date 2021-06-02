a65be90fb58a8d9183f158180fc75a77474e5a45
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-06-02 19:00:35 +0900

    Display generated playlist url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,6 +52,14 @@ function App() {
     let generatedPlaylistTotalDuration = 0;
     generatedPlaylist.forEach((video) => (generatedPlaylistTotalDuration += video.stats.duration / 60));
 
+    let generatedPlaylistUntitledUrl = "https://www.youtube.com/watch_videos?video_ids="
+    for (let i = 0; i < generatedPlaylist.length; ++i) {
+        if (i >= 50) break;
+        const video = generatedPlaylist[i];
+        generatedPlaylistUntitledUrl+=`${video.id},` ;
+    }
+    generatedPlaylistUntitledUrl+="&disable_polymer=true"
+
     return (
         <div className="App">
             <h1>Time Optimal Youtube Playlist Generator</h1>
@@ -84,8 +92,11 @@ function App() {
                     <h3>Generated Playlist:</h3>
                     <ul>{generatedPlaylistItems}</ul>
                     <h4>Total Duration: {generatedPlaylistTotalDuration.toFixed(2)} minutes</h4>
+                    <a href={generatedPlaylistUntitledUrl} target="_blank" rel="noopener noreferrer">View Playlist on Youtube</a>
                 </div>
-            ) : <br/>}
+            ) : (
+                <br />
+            )}
         </div>
     );
 }
