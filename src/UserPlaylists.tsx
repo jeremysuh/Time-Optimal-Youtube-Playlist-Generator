3a44f7a56a38ccca383ccc21fa2b72b2e241c7a9
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-07-14 18:51:58 +0900

    Display playlists in date order

--- src/UserPlaylists.tsx
+++ src/UserPlaylists.tsx
@@ -9,11 +9,16 @@ interface UserPlaylistsProps {
 }
 
 const UserPlaylists = ({ initialLoad, playlists, deletePlaylist, editPlaylist }: UserPlaylistsProps) => {
+
+    const sortedPlaylist = playlists.sort((playlistA, playlistB) => { //newest to oldest
+        return new Date(playlistB.createdOn).getTime() - new Date(playlistA.createdOn).getTime()
+    })
+ 
     return initialLoad ? (
         <div>
             <h4>Playlists:</h4>
             {playlists.length > 0
-                ? playlists.map((playlist: any) => (
+                ? sortedPlaylist.map((playlist: any) => (
                       <PlaylistPanel
                           key={playlist.id}
                           playlist={playlist}
