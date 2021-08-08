9ed29bcbeaac3a6b143a241bc0e77e264c2f0b4b
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-08-08 14:11:45 +0900

    Update db pool

--- server/db/db.ts
+++ server/db/db.ts
@@ -8,6 +8,7 @@ const pool =
               password: process.env.DB_PASSWORD,
               host: process.env.DB_HOST,
               database: process.env.DB_NAME,
+              ssl: process.env.NODE_ENV === "development" ? false : { rejectUnauthorized: false },
               port: 5432,
           })
         : new Pool({
