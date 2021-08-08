569aced6f607dc935b10238e4312f15a40a1844e
Jeung Hwan Suh <jeunghwansuh@gmail.com>
2021-08-08 14:30:41 +0900

    Update db pool

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,20 +1,13 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const pool =
-    process.env.NODE_ENV === "development"
-        ? new Pool({
-              user: process.env.DB_USER,
-              password: process.env.DB_PASSWORD,
-              host: process.env.DB_HOST,
-              database: process.env.DB_NAME,
-              ssl: process.env.NODE_ENV === "development" ? false : { rejectUnauthorized: false },
-              port: 5432,
-          })
-        : new Pool({
-              connectionString: process.env.DATABASE_URL,
-              ssl: { rejectUnauthorized: false },
-              port: 5432,
-          });
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    ssl: process.env.NODE_ENV === "development" ? false : { rejectUnauthorized: false },
+    port: 5432,
+});
 
 export = pool;
